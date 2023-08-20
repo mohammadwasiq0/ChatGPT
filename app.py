de0402238419ad00bec14b100d9d7f7c67d414c7
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2023-08-20 15:04:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  text = st.text_area('Enter text:', 'Tell me about Aligarh Muslim University')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
