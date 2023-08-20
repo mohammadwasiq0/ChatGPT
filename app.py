a2e1f889be52be0e42d0f175ca9a533fc3452847
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2023-08-20 16:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from langchain.llms import OpenAI
 st.title('🦜🔗 Quickstart App')
 st.write("## Connect me on Linkedin [Link](https://www.linkedin.com/in/mohammadwasiq0/)")
 st.write("### [Get API Link](https://platform.openai.com/account/api-keys?ref=blog.streamlit.io)")
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+# openai_api_key = st.sidebar.text_input('OpenAI API Key')
+openai_api_key = "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"
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
   st.info(llm(input_text))
